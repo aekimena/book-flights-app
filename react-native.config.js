d5b838c3333d2483b170879342c2ea11665bd890
Arinze Emmanuel <arinzemmanuel23@gmail.com>
2025-07-11 20:20:51 +0100

    fix: workflow

---
+++ react-native.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  dependencies: {
+    '@react-native-community/datetimepicker': {
+      platforms: {
+        android: {
+          codegenConfig: null,
+        },
+      },
+    },
+    'react-native-gesture-handler': {
+      platforms: {
+        android: {
+          codegenConfig: null,
+        },
+      },
+    },
+  },
+};
